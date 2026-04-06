80fa0adb94d614fa1045a71c8612be793ea283c3
Prakash Raj <prakashumapathi07@gmail.com>
2026-04-06 12:18:20 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        System.out.println("Multiplication of " + a + " and " + b + " is: " + (a * b));
+    }
+}
